f3bf3369348291b2ac502d89ec4b6abd96d5186a
Pablo Diaz <pablodiazuboe@gmail.com>
2016-02-26 18:37:12 -0300

    Test app improvements

--- sample-app/src/main/java/com/puboe/customsnackbar/SampleActivity.java
+++ sample-app/src/main/java/com/puboe/customsnackbar/SampleActivity.java
@@ -2,10 +2,12 @@ package com.puboe.customsnackbar;
 
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.puboe.snackbar.CustomSnackbar;
 
@@ -19,7 +21,17 @@ public class SampleActivity extends AppCompatActivity {
     }
 
     public void showCustomSnackbar(View view) {
-        CustomSnackbar.make(view, "This is a really long text to test how this snackbar looks with plenty of text to show", Snackbar.LENGTH_INDEFINITE).show();
+        CustomSnackbar.make(view, "This is a really long text to test how this snackbar looks with plenty of text to show", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(view.getContext(), "Dismiss!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setTextColor(ContextCompat.getColor(view.getContext(), R.color.light_primary_color))
+                .setActionTextColor(ContextCompat.getColor(view.getContext(), R.color.accent_color))
+                .setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.dark_primary_color))
+                .show();
     }
 
     public void showCustomSnackbarWithCustomView(View view) {
